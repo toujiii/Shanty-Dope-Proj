6d0053360c30e9e712df09cd87b8d8014a744fee
Gareth Manzano <157490038+toujiii@users.noreply.github.com>
2025-08-22 00:57:03 +0800

    header, footer, landing

---
+++ CareToFund/footer.php
@@ -0,0 +1,22 @@
+<?php 
+    echo '
+        <footer class="" style="background-color: #1B3C53;">
+            <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center py-3">
+                <div class="container p-0 text-center mb-3 mb-md-0">
+                    <a class="py-2" href="#">
+                        <img src="img/website_logo.png" alt="CareToFund Logo" width="90" height="90">
+                    </a>
+                </div>
+                <div class="container d-flex flex-column flex-md-row justify-content-center align-items-center px-0 py-2 gap-4">
+                    <a href="" class="text-decoration-none text-white">Home</a> 
+                    <a href="" class="text-decoration-none text-white">Charities</a> 
+                    <a href="" class="text-decoration-none text-white">Sign Up</a>
+                    <a href="" class="text-decoration-none text-white">Sign In</a>
+                </div>
+            </div>
+            <div class="container text-center py-1">
+                <p class="mb-0" style="color: white; font-size: 0.8rem;">© 2025 CareToFund. All rights reserved.</p>
+            </div>
+        </footer>
+    ';
+?>
--- CareToFund/index.php
+++ CareToFund/index.php
@@ -1,5 +1,5 @@
 <?php
-    echo "Hello, World!";
+    // echo "Hello, World!";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,9 +7,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CareToFund</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link href="bootstrap/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/body_styles.css">
+
 </head>
 <body>
-    
+<?php include 'header.php'; ?>
+
+<div style="min-height: 90vh; background-color: #456882; background-image: url('img/charity-front.jpg'); background-size: cover; background-position: center;">
+s
+</div>
+
+<?php include 'footer.php'; ?>
 </body>
 </html>
