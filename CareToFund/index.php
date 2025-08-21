99d9863912ca0e1133491ab3a5ac47f4145c02cb
Gareth Manzano <157490038+toujiii@users.noreply.github.com>
2025-08-21 14:20:47 +0800

    more

---
+++ CareToFund/index.php
@@ -0,0 +1,16 @@
+<?php
+// This is a PHP comment
+    echo "Hello, World!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CareToFund</title>
+    <link href="bootstrap/bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Shanty-Dope-Proj.
 ITELEC 4100
+
+Notes:
+
+Font Style: Ubuntu
+Pallete:
+    #1B3C53
+    #456882
+    #D2C1B6
+    #F9F3EF
