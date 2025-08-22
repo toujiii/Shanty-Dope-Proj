35c787a79d964c0f082dcd7d990854f1f02a3c5c
Jay Laurence Semacio <165934452+XxJLSxX@users.noreply.github.com>
2025-08-22 17:34:07 +0800

    Puta

---
+++ CareToFund/app/Routes/web.php
@@ -0,0 +1,6 @@
+<?php
+// Define all routes here
+$router->add('GET', '/', [HomeController::class, 'index']);
+
+// Add more routes as needed
+?>
--- CareToFund/index.php
+++ CareToFund/index.php
@@ -2,24 +2,17 @@
 //Router ito, dito daraan mga request
 
 require_once 'app/Routes/Router.php';
-require_once 'app/Controllers/UserController.php';
-require_once 'app/Controllers/HomeController.php';
-
+require_once 'app/Routes/web.php'; // Load all routes from a separate file
 use CareToFund\Controllers\Router;
-use CareToFund\Controllers\UserController;
-use CareToFund\Controllers\HomeController;
+
 
 $router = new Router();
-$router->add('GET', '/', [HomeController::class, 'index']);
-// $router->add('GET', '/users', [UserController::class, 'index']);
-// $router->add('GET', '/users/show', [UserController::class, 'show']);
-
-
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = rtrim($uri, '/');
-if ($uri === '/Shanty-Dope-Proj/CareToFund' || $uri === '/Shanty-Dope-Proj/CareToFund/' || $uri === '/Shanty-Dope-Proj/CareToFund/index.php' || $uri === '') {
+if ($uri === '/Shanty-Dope-Proj/CareToFund' || $uri === '/Shanty-Dope-Proj/CareToFund/' || $uri === '') {
     $uri = '/';
 }
 
 $router->dispatch($method, $uri);
+?>
