e88bddcd5fab0bdd2bcf327221333c44a319145c
Gareth Manzano <157490038+toujiii@users.noreply.github.com>
2025-08-23 15:27:42 +0800

    admin styles

---
+++ CareToFund/resources/views/components/admin_charity_card.php
@@ -0,0 +1,57 @@
+
+<div class="container">
+    <div class="container bg-light my-3 px-4 py-2 shadow" style=" border-radius: 12px;">
+        <div class="container ">
+            <div class="d-flex  align-items-center gap-4">
+                <p class=" fs-6 fw-bold my-2 d-flex align-items-center gap-2" style="color: #1b3c53;">
+                    <i class="bi bi-person-circle fs-1"></i>
+                    John Doe
+                </p>
+                
+                <p class="fs-6 m-0 ms-auto fw-bold" style="color: #1b3c53; border-radius: 15px;">
+                    (C1001)
+                </p>
+            </div>
+            <p class="fs-6 m-0 text-white px-3 mb-2" style="background-color: #ffbd59; border-radius: 15px; width: fit-content;">
+                    On Going
+                </p>
+            <p class="text-dark fs-6 m-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <p class="m-0 pb-3" style="font-size: 0.9rem; color: #848484ff;">
+                3:10 PM Jan 15, 2025
+            </p>
+            <div class="progress">
+                <div class="progress-bar progress-bar-striped progress-bar-animated"
+                    role="progressbar"
+                    aria-label="Charity Progress"
+                    style="width: 25%; background-color: #1b3c53;"
+                    aria-valuenow="25"
+                    aria-valuemin="0"
+                    aria-valuemax="100">
+                    25%
+                </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-between">
+                <p class=" mt-2 mb-2" style="font-size: 0.9rem; color: #1b3c53;">
+                    <span class="fw-bold">₱ 400.00</span> / ₱ 3,000.00
+                </p>
+                <p class="m-0 fw-bold" style="font-size: 0.9rem; color: #1b3c53;">
+                    5 Days Left...
+                </p>
+            </div>
+            <div class="d-flex justify-content-between align-items-center my-3">
+                <button class="btn btn-transparent fs-6  fw-bold  p-0 d-flex align-items-center gap-2 m-0" style="color: #1b3c53;">
+                    <i class="bi bi-people-fill fs-5"></i>
+                    View Donators
+                </button>
+                <button class="btn btn-danger px-3 py-1" style="border-radius: 10px; font-size: 0.9rem;">
+                    Abort Charity
+                </button>
+            </div>
+            
+
+        </div>
+
+    </div>
+</div>
